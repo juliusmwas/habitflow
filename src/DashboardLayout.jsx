06c2246227ba04c7f4d76a3e440249eff63625c5
juliusmwass <juliusmwass100@gmail.com>
2025-10-02 08:25:12 +0300

    k

--- src/DashboardLayout.jsx
+++ src/DashboardLayout.jsx
@@ -1,7 +1,7 @@
 // src/DashboardLayout.jsx
 import React from "react";
 import { Outlet } from "react-router-dom";
-import Sidebar from "./Components/Sidebar";
+import Sidebar from "./Components/Sidebar.jsx";
 import "./DashboardLayout.css";
 
 export default function DashboardLayout() {
