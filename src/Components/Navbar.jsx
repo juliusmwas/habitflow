31d0901f333914be12b962fb27f6043592ec6d25
juliusmwass <juliusmwass100@gmail.com>
2025-10-04 10:45:12 +0300

    Added a navbar

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,25 @@
+// src/components/Navbar.jsx
+import React from "react";
+import { IoMenu } from "react-icons/io5";
+import { FaRegBell } from "react-icons/fa";
+import { FaRegUserCircle } from "react-icons/fa";
+
+import "./Navbar.css";
+
+export default function Navbar({ onMenuClick }) {
+  return (
+    <header className="navbar">
+      <div className="navbar-left">
+        <button className="menu-btn" onClick={onMenuClick}>
+          <IoMenu size={24} />
+        </button>
+        <h1 className="navbar-brand">HabitFlow</h1>
+      </div>
+
+      <div className="navbar-right">
+        <FaRegBell className="navbar-icon" size={20} />
+        <FaRegUserCircle className="navbar-icon" size={22} />
+      </div>
+    </header>
+  );
+}
--- src/DashboardLayout.jsx
+++ src/DashboardLayout.jsx
@@ -1,16 +1,23 @@
 // src/DashboardLayout.jsx
-import React from "react";
+import React, { useState } from "react";
 import { Outlet } from "react-router-dom";
 import Sidebar from "./Components/Sidebar.jsx";
+import Navbar from "./Components/Navbar.jsx";
 import "./DashboardLayout.css";
 
 export default function DashboardLayout() {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
   return (
-    <div className="dashboard-layout">
-      <Sidebar />
-      <main className="dashboard-main">
-        <Outlet />
-      </main>
+    <div className="dashboard-container">
+      <Navbar onMenuClick={() => setSidebarOpen(true)} />
+
+      <div className="dashboard-body">
+        <Sidebar isOpen={sidebarOpen} onClose={() => setSidebarOpen(false)} />
+        <main className="dashboard-main">
+          <Outlet />
+        </main>
+      </div>
     </div>
   );
 }
